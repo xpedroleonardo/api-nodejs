a499348b8bcd3b3d4753882a431266c42d0c1f5c
xpedroleonardo <aispedroleonardo@gmail.com>
2022-01-26 21:40:40 -0300

    creating authentication settings

--- src/routes.ts
+++ src/routes.ts
@@ -19,4 +19,7 @@ route.get("/details/:id", selectOneExample); //List One
 route.put("/update/:id", multer(multerConfig).single("avatar"), updateExample); //Update
 route.delete("/delete/:id", deleteExample); //Delete
 
+route.post("/auth/"); //Create Auth
+route.get("/auth/"); //List Auth
+
 export default route;
